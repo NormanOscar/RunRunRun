cdcc533306692e9e65d0be889de9bf8ff9a374a3
Stefan Norman <stefan.norman@bricco.se>
2023-03-16 08:21:36 +0100

    Linting

--- app/screens/RecordScreen.js
+++ app/screens/RecordScreen.js
@@ -1,37 +1,43 @@
-import { useEffect, useState } from 'react';
-import { Text, View, Pressable, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { styles } from '../style/styles.js';
+import { useEffect, useState } from "react";
+import { Text, View, Pressable, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { styles } from "../style/styles.js";
 import { useRoute } from "@react-navigation/native";
-import useTimer from 'easytimer-react-hook';
-import { format } from 'date-fns';
-import * as Location from 'expo-location';
-import haversine from 'haversine';
+import useTimer from "easytimer-react-hook";
+import { format } from "date-fns";
+import * as Location from "expo-location";
+import haversine from "haversine";
 
 export default function RecordScreen({ navigation }) {
-
   const initialLocation = useRoute().params?.location;
 
   const [mapRegion, setmapRegion] = useState({
     latitude: initialLocation.coords.latitude,
     longitude: initialLocation.coords.longitude,
     latitudeDelta: 0.008,
-    longitudeDelta: 0.008
+    longitudeDelta: 0.008,
   });
 
-  const [startGeo, setStartGeo] = useState('');
+  const [startGeo, setStartGeo] = useState("");
   useEffect(() => {
     (async () => {
-      let geocode = await Location.reverseGeocodeAsync({latitude: initialLocation.coords.latitude, longitude: initialLocation.coords.longitude})
-      let geoStr = geocode[0].city + ', ' + geocode[0].country;
+      let geocode = await Location.reverseGeocodeAsync({
+        latitude: initialLocation.coords.latitude,
+        longitude: initialLocation.coords.longitude,
+      });
+      let geoStr = geocode[0].city + ", " + geocode[0].country;
       setStartGeo(geoStr);
     })();
   }, []);
 
   const [timer, isTargetAchieved] = useTimer();
-  const [timerState, setTimerState] = useState('stopped');
-  timer.addEventListener('started', () => {setTimerState('running')});
-  timer.addEventListener('paused', () => {setTimerState('paused')});
+  const [timerState, setTimerState] = useState("stopped");
+  timer.addEventListener("started", () => {
+    setTimerState("running");
+  });
+  timer.addEventListener("paused", () => {
+    setTimerState("paused");
+  });
 
   const [startDate, setStartDate] = useState();
   const [startTime, setStartTime] = useState();
@@ -40,71 +46,78 @@ export default function RecordScreen({ navigation }) {
     const date = new Date();
     setStartDate(format(date, "dd MMMM yyyy"));
     setStartTime(format(date, "HH:mm"));
-  }
+  };
 
   const [runListener, setRunListener] = useState();
   const [allPositions, setAllPositions] = useState([]);
   const startPosition = {
     latitude: initialLocation.coords.latitude,
-    longitude: initialLocation.coords.longitude
+    longitude: initialLocation.coords.longitude,
   };
-  
+
   const [totalDistance, setTotalDistance] = useState(0);
-  
+
   const enableWatchPosition = async () => {
     allPositions.push(startPosition);
     setTotalDistance(0);
     const watchOptions = {
-      accuracy:Location.Accuracy.High,
+      accuracy: Location.Accuracy.High,
       timeInterval: 1000,
-      distanceInterval: 1
+      distanceInterval: 1,
     };
-    setRunListener(await Location.watchPositionAsync
-      (
-        watchOptions, 
-        onPositionChange
-      )
+    setRunListener(
+      await Location.watchPositionAsync(watchOptions, onPositionChange)
     );
   };
-      
+
   const onPositionChange = (locationCoords) => {
-    allPositions.push({latitude: locationCoords.coords.latitude, longitude: locationCoords.coords.longitude});
+    allPositions.push({
+      latitude: locationCoords.coords.latitude,
+      longitude: locationCoords.coords.longitude,
+    });
     updateDistance(locationCoords.coords);
     startPosition.latitude = locationCoords.coords.latitude;
     startPosition.longitude = locationCoords.coords.longitude;
   };
-  
+
   const updateDistance = (currentPosition) => {
     const start = {
       latitude: startPosition.latitude,
-      longitude: startPosition.longitude
+      longitude: startPosition.longitude,
     };
     const end = {
       latitude: currentPosition.latitude,
-      longitude: currentPosition.longitude
+      longitude: currentPosition.longitude,
     };
-    let latestDistance = haversine(start, end, {unit: 'km'});
-    setTotalDistance(prev => prev + latestDistance);
+    let latestDistance = haversine(start, end, { unit: "km" });
+    setTotalDistance((prev) => prev + latestDistance);
   };
 
   const calculatePace = () => {
     if (totalDistance == 0) {
-      return '0:00';
-    };
-    const pace = (timer.getTotalTimeValues().seconds)/totalDistance;
-    let paceStr = Math.floor(pace/60) + ':' + Math.round(pace%60).toString().padStart(2, '0');
+      return "0:00";
+    }
+    const pace = timer.getTotalTimeValues().seconds / totalDistance;
+    let paceStr =
+      Math.floor(pace / 60) +
+      ":" +
+      Math.round(pace % 60)
+        .toString()
+        .padStart(2, "0");
     return paceStr;
   };
   const disableWatchPositon = () => {
     runListener.remove();
-    allPositions.splice[allPositions.length-1];
+    allPositions.splice[allPositions.length - 1];
   };
   return (
-    <View style={[styles.container, {backgroundColor: 'white'}]}>
-      {timerState == 'stopped' && (
-        <View style={{
-          flex: 1,
-        }}>
+    <View style={[styles.container, { backgroundColor: "white" }]}>
+      {timerState == "stopped" && (
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
           <View style={styles.backBtnContainer}>
             <Pressable onPress={() => navigation.goBack()}>
               <Text style={styles.backBtnText}>Back</Text>
@@ -113,12 +126,12 @@ export default function RecordScreen({ navigation }) {
           <View style={styles.map}>
             <MapView
               style={{
-                width: Dimensions.get('window').width,
-                height: (Dimensions.get('window').height - 300)
+                width: Dimensions.get("window").width,
+                height: Dimensions.get("window").height - 300,
               }}
-              provider={ PROVIDER_GOOGLE } 
+              provider={PROVIDER_GOOGLE}
               initialRegion={mapRegion}
-              mapType={'standard'}
+              mapType={"standard"}
               showsUserLocation={true}
               showsMyLocationButton={true}
               showsCompass={true}
@@ -127,97 +140,114 @@ export default function RecordScreen({ navigation }) {
             />
           </View>
           <View style={[styles.btnContainer, { flex: 0.4 }]}>
-            <Pressable style={styles.btn} onPress={() => {
-              timer.start();
-              enableWatchPosition();
-              setDate();
-            }
-            }>
+            <Pressable
+              style={styles.btn}
+              onPress={() => {
+                timer.start();
+                enableWatchPosition();
+                setDate();
+              }}
+            >
               <Text style={styles.btnText}>Start</Text>
             </Pressable>
           </View>
         </View>
       )}
-      {['paused', 'running'].includes(timerState) && (
-        <View style={{
-          flex: 1,
-        }}>
+      {["paused", "running"].includes(timerState) && (
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
           <View style={styles.measureContainer}>
-            {timerState == 'paused' && (
+            {timerState == "paused" && (
               <View style={styles.stoppedAlert}>
                 <Text style={styles.stoppedAlertText}>Stopped</Text>
               </View>
             )}
             <View style={styles.recordFields}>
               <Text style={styles.recordHeaders}>TIME</Text>
-              <Text style={styles.recordDigits}>{timer.getTimeValues().toString()}</Text>
+              <Text style={styles.recordDigits}>
+                {timer.getTimeValues().toString()}
+              </Text>
             </View>
             <View style={styles.recordFields}>
               <Text style={styles.recordHeaders}>Distance (KM)</Text>
-              <Text style={styles.recordDigits}>{totalDistance.toFixed(2)}</Text>
+              <Text style={styles.recordDigits}>
+                {totalDistance.toFixed(2)}
+              </Text>
             </View>
             <View style={styles.recordFields}>
               <Text style={styles.recordHeaders}>AVG PACE (KM)</Text>
               <Text style={styles.recordDigits}>{calculatePace()}</Text>
             </View>
             <View style={[styles.btnContainer, { flex: 0.5 }]}>
-              {timerState == 'running' && (
+              {timerState == "running" && (
                 <Pressable
                   style={[
-                    styles.btn, styles.FinishBtn,
+                    styles.btn,
+                    styles.FinishBtn,
                     {
-                      backgroundColor: '#FF5F5F',
-                    }
+                      backgroundColor: "#FF5F5F",
+                    },
                   ]}
                   onPress={() => {
                     disableWatchPositon();
                     timer.pause();
-                  }}>
+                  }}
+                >
                   <Text style={styles.btnText}>Stop</Text>
                 </Pressable>
               )}
-              {timerState == 'paused' && (
-                <View style={[
-                  styles.btnContainer,
-                  {
-                    paddingTop: 10,
-                    flexDirection: 'row'
-                  }
-                ]}>
-                  <Pressable style={[
-                    styles.btn,
+              {timerState == "paused" && (
+                <View
+                  style={[
+                    styles.btnContainer,
                     {
-                      backgroundColor: '#44fc00',
-                      marginHorizontal: 10,
-                      width: 180
-                    }
+                      paddingTop: 10,
+                      flexDirection: "row",
+                    },
                   ]}
+                >
+                  <Pressable
+                    style={[
+                      styles.btn,
+                      {
+                        backgroundColor: "#44fc00",
+                        marginHorizontal: 10,
+                        width: 180,
+                      },
+                    ]}
                     onPress={() => {
                       timer.start();
                       enableWatchPosition();
-                    }}>
+                    }}
+                  >
                     <Text style={styles.btnText}>Resume</Text>
                   </Pressable>
                   <Pressable
                     style={[
                       styles.btn,
                       {
-                        backgroundColor: '#FF5F5F',
+                        backgroundColor: "#FF5F5F",
                         marginHorizontal: 10,
-                        width: 180
-                      }
+                        width: 180,
+                      },
                     ]}
                     onPress={() => {
-                      navigation.navigate('Save', { runData: 
-                        {date: startDate, 
-                          startTime: startTime, 
-                          startGeo: startGeo, 
-                          duration: timer.getTimeValues().toString(), 
-                          distance: totalDistance.toFixed(2), 
-                          avgPace: calculatePace(), 
-                          positions: allPositions
-                        }});
-                    }}>
+                      navigation.navigate("Save", {
+                        runData: {
+                          date: startDate,
+                          startTime: startTime,
+                          startGeo: startGeo,
+                          duration: timer.getTimeValues().toString(),
+                          distance: totalDistance.toFixed(2),
+                          avgPace: calculatePace(),
+                          positions: allPositions,
+                        },
+                      });
+                    }}
+                  >
                     <Text style={styles.btnText}>Finish</Text>
                   </Pressable>
                 </View>
--- app/screens/RunScreen.js
+++ app/screens/RunScreen.js
@@ -1,26 +1,26 @@
-import { useRoute } from '@react-navigation/native';
-import { Text, View, Pressable, Dimensions, Modal } from 'react-native';
-import { styles } from '../style/styles.js';
-import { Entypo } from '@expo/vector-icons';
-import MapView, { PROVIDER_GOOGLE, Polyline, Marker } from 'react-native-maps';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useState, useEffect, useRef } from 'react';
+import { useRoute } from "@react-navigation/native";
+import { Text, View, Pressable, Dimensions, Modal } from "react-native";
+import { styles } from "../style/styles.js";
+import { Entypo } from "@expo/vector-icons";
+import MapView, { PROVIDER_GOOGLE, Polyline, Marker } from "react-native-maps";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useState, useEffect, useRef } from "react";
 
 export default function RunScreen({ navigation }) {
-
   const route = useRoute();
   const runData = route.params?.runData;
   console.log(runData.positions);
-  
+
   const [modalVisible, setModalVisible] = useState(false);
-  
+
   const [allRuns, setAllRuns] = useState([]);
-  
+
   const _map = useRef();
   useEffect(() => {
     (() => {
-    
-      runData.positions.map(position => {allRuns.push(position)});
+      runData.positions.map((position) => {
+        allRuns.push(position);
+      });
       allRuns.pop();
       console.log(allRuns);
 
@@ -31,23 +31,25 @@ export default function RunScreen({ navigation }) {
           bottom: 80,
           left: 80,
         },
-        animated: true
+        animated: true,
       });
     })();
   }, []);
 
   const deleteRun = async () => {
     let savedData = [];
-  
+
     try {
-      savedData = JSON.parse(await AsyncStorage.getItem('savedData'));
+      savedData = JSON.parse(await AsyncStorage.getItem("savedData"));
     } catch (error) {}
-    const currentRun = savedData.filter(run => run.runIndex == runData.runIndex );
+    const currentRun = savedData.filter(
+      (run) => run.runIndex == runData.runIndex
+    );
     savedData.splice(currentRun.runIndex, 1);
     try {
-      await AsyncStorage.setItem('savedData', JSON.stringify(savedData));
+      await AsyncStorage.setItem("savedData", JSON.stringify(savedData));
     } catch (error) {}
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -55,25 +57,30 @@ export default function RunScreen({ navigation }) {
         animationType="slide"
         transparent={true}
         visible={modalVisible}
-        onRequestClose={() => { setModalVisible(!modalVisible) }}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Do you want to delete this run?</Text>
-            <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.modalText}>
+              Do you want to delete this run?
+            </Text>
+            <View style={{ flexDirection: "row" }}>
               <Pressable
                 style={[styles.modalBtns, styles.cancelBtn]}
-                onPress={() => setModalVisible(!modalVisible)}>
-                <Text
-                  style={styles.modalBtnsText}>Cancel</Text>
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.modalBtnsText}>Cancel</Text>
               </Pressable>
               <Pressable
                 style={[styles.modalBtns, styles.deleteBtn]}
                 onPress={() => {
                   deleteRun();
                   setModalVisible(!modalVisible);
-                  navigation.navigate('Home');
-                }}>
+                  navigation.navigate("Home");
+                }}
+              >
                 <Text style={styles.modalBtnsText}>Delete</Text>
               </Pressable>
             </View>
@@ -86,59 +93,112 @@ export default function RunScreen({ navigation }) {
           <Text style={styles.backBtnText}>Back</Text>
         </Pressable>
       </View>
-      <View style={{ alignItems: 'center', backgroundColor: 'white', flex: 1 }}>
-        <View style={{ width: '90%', marginTop: 10 }}>
+      <View style={{ alignItems: "center", backgroundColor: "white", flex: 1 }}>
+        <View style={{ width: "90%", marginTop: 10 }}>
           <Text style={{ fontSize: 25, paddingBottom: 5 }}>{runData.name}</Text>
           <View style={styles.runDateTime}>
             <Entypo name="calendar" size={16} color="black" />
-            <Text style={styles.runDateTimeText}>{runData.date} at {runData.startTime}</Text>
+            <Text style={styles.runDateTimeText}>
+              {runData.date} at {runData.startTime}
+            </Text>
             <Entypo name="location-pin" size={16} color="black" />
             <Text style={styles.runDateTimeText}>{runData.startGeo}</Text>
           </View>
-          {runData.desc != '' && (
-            <View style={{marginTop: 20}}>
-              <Text style={{fontSize:18, color: 'grey'}}>{runData.desc}</Text>
+          {runData.desc != "" && (
+            <View style={{ marginTop: 20 }}>
+              <Text style={{ fontSize: 18, color: "grey" }}>
+                {runData.desc}
+              </Text>
             </View>
           )}
         </View>
-        <View style={{marginTop: 10, width:'100%', flexDirection:'row', alignItems:'center'}}>
-            <Text style={{fontSize:18, marginHorizontal:20}}>How it felt:</Text>
-            {runData.feeling == 'good' && (
-            <Entypo name="emoji-happy" size={35} color="black" backgroundColor='#32cd32' />
-            )}
-            {runData.feeling == 'medium' && (
-            <Entypo name="emoji-neutral" size={35} color="black" backgroundColor='#ffff00' />
-            )}
-            {runData.feeling == 'sad' && (
-            <Entypo name="emoji-sad" size={35} color="black" backgroundColor='#FF5F5F' />
-            )}
+        <View
+          style={{
+            marginTop: 10,
+            width: "100%",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 18, marginHorizontal: 20 }}>
+            How it felt:
+          </Text>
+          {runData.feeling == "good" && (
+            <Entypo
+              name="emoji-happy"
+              size={35}
+              color="black"
+              backgroundColor="#32cd32"
+            />
+          )}
+          {runData.feeling == "medium" && (
+            <Entypo
+              name="emoji-neutral"
+              size={35}
+              color="black"
+              backgroundColor="#ffff00"
+            />
+          )}
+          {runData.feeling == "sad" && (
+            <Entypo
+              name="emoji-sad"
+              size={35}
+              color="black"
+              backgroundColor="#FF5F5F"
+            />
+          )}
         </View>
-        <View style={{ width: '100%', marginTop: 10 }}>
-          <View style={{ alignItems: 'center' }}>
-            <View style={{ alignItems: 'center', width: '90%', borderBottomColor: 'lightgrey', borderBottomWidth: 1, paddingVertical: 10 }}>
+        <View style={{ width: "100%", marginTop: 10 }}>
+          <View style={{ alignItems: "center" }}>
+            <View
+              style={{
+                alignItems: "center",
+                width: "90%",
+                borderBottomColor: "lightgrey",
+                borderBottomWidth: 1,
+                paddingVertical: 10,
+              }}
+            >
               <Text style={{ fontSize: 20 }}>Time:</Text>
-              <Text style={{ fontSize: 30, letterSpacing: 2 }}>{runData.duration}</Text>
+              <Text style={{ fontSize: 30, letterSpacing: 2 }}>
+                {runData.duration}
+              </Text>
             </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '90%', borderBottomColor: 'lightgrey', borderBottomWidth: 1, paddingHorizontal: 30, paddingVertical: 10 }}>
-              <View style={{ alignItems: 'center', width: 150 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                width: "90%",
+                borderBottomColor: "lightgrey",
+                borderBottomWidth: 1,
+                paddingHorizontal: 30,
+                paddingVertical: 10,
+              }}
+            >
+              <View style={{ alignItems: "center", width: 150 }}>
                 <Text style={{ fontSize: 20 }}>Distance:</Text>
-                <Text style={{ fontSize: 30, letterSpacing: 2 }}>{runData.distance} km</Text>
+                <Text style={{ fontSize: 30, letterSpacing: 2 }}>
+                  {runData.distance} km
+                </Text>
               </View>
               <View style={styles.verticleLine}></View>
-              <View style={{ alignItems: 'center', width: 150 }}>
+              <View style={{ alignItems: "center", width: 150 }}>
                 <Text style={{ fontSize: 20 }}>Pace:</Text>
-                <Text style={{ fontSize: 30, letterSpacing: 2 }}>{runData.avgPace} /km</Text>
+                <Text style={{ fontSize: 30, letterSpacing: 2 }}>
+                  {runData.avgPace} /km
+                </Text>
               </View>
             </View>
             <View>
               <MapView
                 style={{
                   width: 420,
-                  height: 450
+                  height: 450,
                 }}
                 ref={_map}
                 provider={PROVIDER_GOOGLE}
-                mapType={'standard'}
+                mapType={"standard"}
                 showsCompass={true}
                 zoomEnabled={true}
                 showsBuildings={true}
@@ -150,19 +210,22 @@ export default function RunScreen({ navigation }) {
                 />
                 <Marker
                   coordinate={runData.positions[0]}
-                  title='Start'
-                  icon={require('../assets/startIcon.png')}
+                  title="Start"
+                  icon={require("../assets/startIcon.png")}
                 />
-                <Marker 
+                <Marker
                   coordinate={runData.positions[runData.positions.length - 1]}
-                  title='End'
-                  icon={require('../assets/finishIcon.png')}
+                  title="End"
+                  icon={require("../assets/finishIcon.png")}
                 />
               </MapView>
             </View>
           </View>
         </View>
-        <Pressable style={{position:'absolute', bottom:45}} onPress={() => setModalVisible(true)}>
+        <Pressable
+          style={{ position: "absolute", bottom: 45 }}
+          onPress={() => setModalVisible(true)}
+        >
           <Text style={styles.openModalBtn}>Delete run</Text>
         </Pressable>
       </View>
